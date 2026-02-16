1683eb62b4780ba37284ede2ee1c4c5ff8a37339
Andrey Zuev <a.zuev@texrus.com>
2026-02-16 18:55:48 +1000

    Added gsap set to points

--- src/components/CircleNavigation.tsx
+++ src/components/CircleNavigation.tsx
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import '../styles/CircleNavigation.scss';
 import { CircleNavigationProps } from '../types';
+import { gsap } from 'gsap';
 
 const CircleNavigation: React.FC<CircleNavigationProps> = ({ periods }) => {
+  const pointsRef = useRef<(HTMLDivElement | null)[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  //
+  useEffect(() => {
+    if (containerRef.current && periods.length >= 2 && periods.length <= 6) {
+      const points = pointsRef.current.filter(Boolean);
+      const angleStep = 360 / periods.length;
+      const radius = containerRef.current.offsetWidth / 2;
+
+      points.forEach((point, index) => {
+        const angle = (index * angleStep + 45) * (Math.PI / 180);
+        const x = radius * Math.cos(angle);
+        const y = radius * Math.sin(angle);
+
+        gsap.set(point, {
+          x: x,
+          y: y,
+          xPercent: -50,
+          yPercent: -50,
+        });
+      });
+    }
+  }, [periods.length]);
+  //
   return (
     <div className="circle-navigation">
-      <div className="circle-navigation__container">
+      <div className="circle-navigation__container" ref={containerRef}>
         <div className="circle-navigation__circle" />
         {periods.map((prd, idx) => (
-          <div className="circle-navigation__point" key={prd.id} onClick={() => {}}>
+          <div
+            className="circle-navigation__point"
+            ref={(el) => {
+              pointsRef.current[idx] = el;
+            }}
+            key={prd.id}
+            onClick={() => {}}>
             <span
               className="circle-navigation__point-label"
               onClick={(e) => {
